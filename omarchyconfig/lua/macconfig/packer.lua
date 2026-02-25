add1c9f62b29bf075384fdf92a79f8224f42105f
ZoZo-182 <zaza021803@gmail.com>
2026-02-24 21:33:07 -0500

    removed playground. updated treesitter's api. using telescope latest releave. added an optional dependency to telescope.

--- omarchyconfig/lua/macconfig/packer.lua
+++ omarchyconfig/lua/macconfig/packer.lua
@@ -8,9 +8,10 @@ return require('packer').startup(function(use)
   use ('wbthomason/packer.nvim')
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	  'nvim-telescope/telescope.nvim', version = '*',
 	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  requires = { {'nvim-lua/plenary.nvim'},
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } }
   }
 
   use({
@@ -21,8 +22,10 @@ return require('packer').startup(function(use)
 	  end
   })
 
+  -- synthwave84
+  -- aether
+
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use ('nvim-treesitter/playground')
   use ('ThePrimeagen/harpoon')
   use ('mbbill/undotree')
   use ('tpope/vim-fugitive')
